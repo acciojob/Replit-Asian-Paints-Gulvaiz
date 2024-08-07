0c702d4517745d211cd0d3f4b494fa7cb4e4ac31
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-07 14:03:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.getElementById('change_button').addEventListener('click', () => {
+    const blockId = document.getElementById('block_id').value;
+    const color = document.getElementById('colour_id').value;
+
+   
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => item.style.backgroundColor = 'transparent');
+
+    // Change the color of the specified grid item
+    const block = document.getElementById(blockId);
+    if (block) {
+        block.style.backgroundColor = color;
+    }
+});
+
+document.getElementById('Reset').addEventListener('click', () => {
+   
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => item.style.backgroundColor = 'transparent');
+});
